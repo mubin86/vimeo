5d1df61778db983a327de86b5c9223ec5d637afb
Mubinur Rahman <rahman.mmubinur@gmail.com>
2021-01-12 14:23:27 +0600

    auth added

--- app.js
+++ app.js
@@ -13,51 +13,64 @@ let client = new Vimeo(
   // process.env.unauth_acess_token
 );
 
-client.request(
-  {
-    method: "GET",
-    path: "/tutorial",
-  },
-  function (error, body, status_code, headers) {
-    if (error) {
-      console.log(error);
+// client.request(
+//   {
+//     method: "GET",
+//     path: "/tutorial",
+//   },
+//   function (error, body, status_code, headers) {
+//     if (error) {
+//       console.log(error);
+//     }
+
+//     console.log(body);
+//   }
+// );
+var scopes, token;
+
+client.generateClientCredentials(
+  ["public", "upload", "video_files"],
+  function (err, response) {
+    if (err) {
+      throw err;
     }
 
-    console.log(body);
+    token = response.access_token;
+
+    console.log(token);
+
+    // Other useful information is included alongside the access token,
+    // which you can dump out to see, or visit our API documentation.
+    //
+    // We include the final scopes granted to the token. This is
+    // important because the user, or API, might revoke scopes during
+    // the authentication process.
+    scopes = response.scope;
+    console.log(scopes);
   }
 );
 
-// client.generateClientCredentials(["public, upload"], function (err, response) {
-//   if (err) {
-//     throw err;
+var url = client.buildAuthorizationEndpoint(
+  "https://beta.somoyinfo.com",
+  scopes,
+  "test123*"
+);
+
+// let file_name = "/home/sayburgh/Downloads/Sample Videos 2.mp4";
+// client.upload(
+//   file_name,
+//   {
+//     name: "sample video",
+//     description: "The description goes here.",
+//   },
+//   function (uri) {
+//     console.log("Your video URI is: " + uri);
+//   },
+//   function (bytes_uploaded, bytes_total) {
+//     var percentage = ((bytes_uploaded / bytes_total) * 100).toFixed(2);
+//     console.log(bytes_uploaded, bytes_total, percentage + "%");
+//   },
+//   function (error) {
+//     console.log("Failed because: " + error);
 //   }
-
-//   var token = response.access_token;
-
-//   // Other useful information is included alongside the access token,
-//   // which you can dump out to see, or visit our API documentation.
-//   //
-//   // We include the final scopes granted to the token. This is
-//   // important because the user, or API, might revoke scopes during
-//   // the authentication process.
-//   var scopes = response.scope;
-// });
-
-let file_name = "/home/sayburgh/Downloads/Sample Videos 2.mp4";
-client.upload(
-  file_name,
-  {
-    name: "Untitled",
-    description: "The description goes here.",
-  },
-  function (uri) {
-    console.log("Your video URI is: " + uri);
-  },
-  function (bytes_uploaded, bytes_total) {
-    var percentage = ((bytes_uploaded / bytes_total) * 100).toFixed(2);
-    console.log(bytes_uploaded, bytes_total, percentage + "%");
-  },
-  function (error) {
-    console.log("Failed because: " + error);
-  }
-);
+// );
